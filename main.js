606f56a276a7bb77e4fa8f2ae344e8476a642dca
Praveenkanth0017 <pkanth984@gamil.com>
2025-08-20 18:48:26 +0530

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 6969;
+
+app.get('/', (req, res) => {
+    res.json({msg: "hello students"});
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`the server is running at http://localhost:${PORT}`);
+});
+
