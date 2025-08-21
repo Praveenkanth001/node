4a3c16c1a0d84d6d0da6991fab23546b0e0c7adc
Praveenkanth0017 <pkanth984@gamil.com>
2025-08-21 14:09:50 +0530

    route structure

--- main.js
+++ main.js
@@ -5,9 +5,27 @@ const PORT = 6969;
 
 app.get('/', (req, res) => {
     res.json({msg: "hello students"});
-  });
+});
 
+// CRUD functionality of movies
+//R - for reading
+app.get('/movies',()=>{
 
+})
+//C - for creating movies
+app.post('/movies',()=>{
+
+})
+
+// U - for updating movies
+app.put('/movies/:id',()=>{
+
+})
+
+// D - for deleting movies
+app.delete('/movies/:id',()=>{
+
+})
 app.listen(PORT, () => {
     console.log(`the server is running at http://localhost:${PORT}`);
 });
